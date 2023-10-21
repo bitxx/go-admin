d946b63c9d0754fd587c7c0563cccbf80d4ebd5a
Jason <>
2023-10-22 00:08:32 +0800

    撤销对postgres的支持，等稳定了在考虑

--- core/storage/database/open.go
+++ core/storage/database/open.go
@@ -2,11 +2,9 @@ package database
 
 import (
 	"gorm.io/driver/mysql"
-	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 var opens = map[string]func(string) gorm.Dialector{
-	"mysql":    mysql.Open,
-	"postgres": postgres.Open,
+	"mysql": mysql.Open,
 }
