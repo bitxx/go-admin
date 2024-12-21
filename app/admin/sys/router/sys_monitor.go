18928338680422cdf5b2d93685a51981fd54d188
Jason <none>
2024-12-21 22:48:55 +0800

    ping、prom规范化

--- app/admin/sys/router/sys_monitor.go
+++ app/admin/sys/router/sys_monitor.go
@@ -2,11 +2,8 @@ package router
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"go-admin/app/admin/sys/apis"
 	"go-admin/core/middleware"
-	"go-admin/core/utils/ginutils"
-	"net/http"
 )
 
 func init() {
@@ -19,16 +16,8 @@ func registerSysMonitorRouter(v1 *gin.RouterGroup) {
 	r := v1.Group("/admin/sys/sys-monitor").Use(middleware.Auth()).Use(middleware.AuthCheckRole())
 	{
 		r.GET("", api.GetMonitor)
-		r.GET("/prom", ginutils.Handler(promhttp.Handler()))
-		//健康检查
-		r.GET("/health", func(c *gin.Context) {
-			c.Status(http.StatusOK)
-		})
-		r.GET("/ping", func(c *gin.Context) {
-			c.JSON(200, gin.H{
-				"message": "ok",
-			})
-		})
+		r.GET("/prom", api.Prom)
+		r.GET("/ping", api.Ping)
 	}
 
 }
