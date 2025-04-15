9be14b6f98c016dbc95e311f200069de09539478
Jason <none>
2025-04-15 17:52:36 +0800

    变更方法名

--- core/utils/fileutils/file.go
+++ core/utils/fileutils/file.go
@@ -27,11 +27,11 @@ func GetExt(fileName string) string {
 	return path.Ext(fileName)
 }
 
-// CheckExist 检查文件是否存在
-func CheckExist(src string) bool {
+// IsFileExist 检查文件是否存在
+func IsFileExist(src string) bool {
 	_, err := os.Stat(src)
 
-	return os.IsNotExist(err)
+	return os.IsExist(err)
 }
 
 // CheckPermission 检查文件权限
@@ -44,7 +44,7 @@ func CheckPermission(src string) bool {
 // IsNotExistMkDir 检查文件夹是否存在
 // 如果不存在则新建文件夹
 func IsNotExistMkDir(src string) error {
-	if exist := !CheckExist(src); exist == false {
+	if exist := IsFileExist(src); exist == false {
 		if err := MkDir(src); err != nil {
 			return err
 		}
