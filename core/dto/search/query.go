5dfdadabb44b97777a5c7f7e9f86158c9f55988c
Jason <none>
2025-07-09 17:41:19 +0800

    pgsql兼容处理

--- core/dto/search/query.go
+++ core/dto/search/query.go
@@ -50,7 +50,7 @@ func ResolveSearchQuery(q interface{}, condition Condition) {
 		case "left":
 			//左关联
 			join := condition.SetJoinOn(t.Type, fmt.Sprintf(
-				"left join `%s` on `%s`.`%s` = `%s`.`%s`",
+				"left join  %s on  %s.%s =  %s.%s",
 				t.Join,
 				t.Join,
 				t.On[0],
@@ -61,7 +61,7 @@ func ResolveSearchQuery(q interface{}, condition Condition) {
 		case "inner":
 			//左关联
 			join := condition.SetJoinOn(t.Type, fmt.Sprintf(
-				"inner join `%s` on `%s`.`%s` = `%s`.`%s`",
+				"inner join %s on %s.%s = %s.%s",
 				t.Join,
 				t.Join,
 				t.On[0],
@@ -70,35 +70,35 @@ func ResolveSearchQuery(q interface{}, condition Condition) {
 			))
 			ResolveSearchQuery(qValue.Field(i).Interface(), join)
 		case "exact", "iexact":
-			condition.SetWhere(fmt.Sprintf("`%s`.`%s` = ?", t.Table, t.Column), []interface{}{qValue.Field(i).Interface()})
+			condition.SetWhere(fmt.Sprintf("%s.%s = ?", t.Table, t.Column), []interface{}{qValue.Field(i).Interface()})
 		case "contains", "icontains":
-			condition.SetWhere(fmt.Sprintf("`%s`.`%s` like ?", t.Table, t.Column), []interface{}{"%" + qValue.Field(i).String() + "%"})
+			condition.SetWhere(fmt.Sprintf("%s.%s like ?", t.Table, t.Column), []interface{}{"%" + qValue.Field(i).String() + "%"})
 		case "leftcontains", "lefticontains":
-			condition.SetWhere(fmt.Sprintf("`%s`.`%s` like ?", t.Table, t.Column), []interface{}{"%" + qValue.Field(i).String()})
+			condition.SetWhere(fmt.Sprintf("%s.%s like ?", t.Table, t.Column), []interface{}{"%" + qValue.Field(i).String()})
 		case "rightcontains", "righticontains":
-			condition.SetWhere(fmt.Sprintf("`%s`.`%s` like ?", t.Table, t.Column), []interface{}{qValue.Field(i).String() + "%"})
+			condition.SetWhere(fmt.Sprintf("%s.%s like ?", t.Table, t.Column), []interface{}{qValue.Field(i).String() + "%"})
 		case "gt":
-			condition.SetWhere(fmt.Sprintf("`%s`.`%s` > ?", t.Table, t.Column), []interface{}{qValue.Field(i).Interface()})
+			condition.SetWhere(fmt.Sprintf("%s.%s > ?", t.Table, t.Column), []interface{}{qValue.Field(i).Interface()})
 		case "gte":
-			condition.SetWhere(fmt.Sprintf("`%s`.`%s` >= ?", t.Table, t.Column), []interface{}{qValue.Field(i).Interface()})
+			condition.SetWhere(fmt.Sprintf("%s.%s >= ?", t.Table, t.Column), []interface{}{qValue.Field(i).Interface()})
 		case "lt":
-			condition.SetWhere(fmt.Sprintf("`%s`.`%s` < ?", t.Table, t.Column), []interface{}{qValue.Field(i).Interface()})
+			condition.SetWhere(fmt.Sprintf("%s.%s < ?", t.Table, t.Column), []interface{}{qValue.Field(i).Interface()})
 		case "lte":
-			condition.SetWhere(fmt.Sprintf("`%s`.`%s` <= ?", t.Table, t.Column), []interface{}{qValue.Field(i).Interface()})
+			condition.SetWhere(fmt.Sprintf("%s.%s <= ?", t.Table, t.Column), []interface{}{qValue.Field(i).Interface()})
 		case "startswith", "istartswith":
-			condition.SetWhere(fmt.Sprintf("`%s`.`%s` like ?", t.Table, t.Column), []interface{}{qValue.Field(i).String() + "%"})
+			condition.SetWhere(fmt.Sprintf("%s.%s like ?", t.Table, t.Column), []interface{}{qValue.Field(i).String() + "%"})
 		case "endswith", "iendswith":
-			condition.SetWhere(fmt.Sprintf("`%s`.`%s` like ?", t.Table, t.Column), []interface{}{"%" + qValue.Field(i).String()})
+			condition.SetWhere(fmt.Sprintf("%s.%s like ?", t.Table, t.Column), []interface{}{"%" + qValue.Field(i).String()})
 		case "in":
-			condition.SetWhere(fmt.Sprintf("`%s`.`%s` in (?)", t.Table, t.Column), []interface{}{qValue.Field(i).Interface()})
+			condition.SetWhere(fmt.Sprintf("%s.%s in (?)", t.Table, t.Column), []interface{}{qValue.Field(i).Interface()})
 		case "isnull":
 			if !(qValue.Field(i).IsZero() && qValue.Field(i).IsNil()) {
-				condition.SetWhere(fmt.Sprintf("`%s`.`%s` isnull", t.Table, t.Column), make([]interface{}, 0))
+				condition.SetWhere(fmt.Sprintf("%s.%s isnull", t.Table, t.Column), make([]interface{}, 0))
 			}
 		case "order":
 			switch strings.ToLower(qValue.Field(i).String()) {
 			case "desc", "asc":
-				condition.SetOrder(fmt.Sprintf("`%s`.`%s` %s", t.Table, t.Column, qValue.Field(i).String()))
+				condition.SetOrder(fmt.Sprintf("%s.%s %s", t.Table, t.Column, qValue.Field(i).String()))
 			}
 		}
 	}
