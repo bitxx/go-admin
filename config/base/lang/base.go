0f26972534ad3e1b646694e8db139c3971a8103c
Jason <none>
2025-11-26 12:36:47 +0800

    新增通用内部格式化错误

--- config/base/lang/base.go
+++ config/base/lang/base.go
@@ -23,6 +23,7 @@ const (
 	DataDeleteCode    = 1009
 	DataDeleteLogCode = 1010
 	DataNotFoundCode  = 1011
+	DataErrLogCode    = 1012
 )
 
 func init() {
@@ -46,4 +47,5 @@ func init() {
 	lang.MsgInfo[DataDeleteCode] = "数据删除失败"
 	lang.MsgInfo[DataDeleteLogCode] = "数据删除失败：%s"
 	lang.MsgInfo[DataNotFoundCode] = "数据不存在"
+	lang.MsgInfo[DataErrLogCode] = "内部错误：%s"
 }
