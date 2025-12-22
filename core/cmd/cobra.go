01430fa55746234c685e6dea363efde049c1c283
Jason <none>
2025-12-22 13:47:45 +0800

    refactor:简化代码

--- core/cmd/cobra.go
+++ core/cmd/cobra.go
@@ -3,8 +3,6 @@ package cmd
 import (
 	"errors"
 	"go-admin/core/cmd/api"
-	"go-admin/core/config"
-	"go-admin/core/utils/log"
 	"go-admin/core/utils/textutils"
 	"os"
 
@@ -12,27 +10,16 @@ import (
 )
 
 var rootCmd = &cobra.Command{
-	Use:          config.ApplicationConfig.Name,
-	Short:        config.ApplicationConfig.Name,
 	SilenceUsage: true,
-	Long:         config.ApplicationConfig.Name,
-	Version:      config.ApplicationConfig.Version,
+	CompletionOptions: cobra.CompletionOptions{
+		DisableDefaultCmd: true,
+	},
 	Args: func(cmd *cobra.Command, args []string) error {
 		if len(args) < 1 {
-			tip()
 			return errors.New(textutils.Red("requires at least one arg"))
 		}
 		return nil
 	},
-	PersistentPreRunE: func(*cobra.Command, []string) error { return nil },
-	Run: func(cmd *cobra.Command, args []string) {
-		tip()
-	},
-}
-
-func tip() {
-	usageStr := `欢迎使用 ` + textutils.Green(config.ApplicationConfig.Name+" "+config.ApplicationConfig.Version) + ` 可以使用 ` + textutils.Red(`-h`) + ` 查看命令`
-	log.Infof("%s\n", usageStr)
 }
 
 func init() {
