21f62e47ad48e3f05028a9a6ce8bdf89610a71a1
Jason <none>
2025-12-24 14:05:42 +0800

    refactor: 删除无效代码

--- core/middleware/auth/jwtauth/jwt.go
+++ core/middleware/auth/jwtauth/jwt.go
@@ -182,8 +182,6 @@ var (
 	// ErrInvalidSigningAlgorithm indicates signing algorithm is invalid, needs to be HS256, HS384, HS512, RS256, RS384 or RS512
 	ErrInvalidSigningAlgorithm = errors.New("invalid signing algorithm")
 
-	ErrInvalidVerificationode = errors.New("验证码错误")
-
 	// ErrNoPrivKeyFile indicates that the given private key is unreadable
 	ErrNoPrivKeyFile = errors.New("private key file unreadable")
 
